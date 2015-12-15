3bb24e28576dbc1fd9a9f75297a33dd0ee9e8caf
Matthew Pfister <matthew.pfister@slalo.com>
2015-12-15 10:56:28 -0600

    adding in debugging

--- scripts/main.js
+++ scripts/main.js
@@ -32,7 +32,7 @@ function getStatusesByFloor(stalls) {
     var floorStatus = [],
         floors      = stalls.statuses;
 
-    console.log('stalls: ', stalls);
+    console.log('stalls: ', stalls['statuses']);
 
     _.keys(floors, function (floor) {
         console.log('floor: ', floor);
